cba7f5fd78e3bc9043362f76ec410c6ba3316926
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-04-08 19:52:25 +0300

    feat : create feedback action

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { feedbackSchema } from "@/constants";
 import { db } from "@/firebase/admin";
+import { google } from "@ai-sdk/google";
+import { generateObject } from "ai";
 
 export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
     const interviews = await db
@@ -10,24 +13,24 @@ export async function getInterviewsByUserId(userId: string): Promise<Interview[]
         .get();
 
     return interviews.docs.map((doc) => ({
-        id : doc.id,
+        id: doc.id,
         ...doc.data()
     })) as Interview[];
 }
 
- /**
- * Fetches the most recent finalized interviews, excluding those of a specified user.
- *
- * This asynchronous function queries the "interviews" collection in the database, 
- * ordering the results by their creation date in descending order. It filters out 
- * interviews associated with the provided user ID and limits the number of results 
- * based on the specified limit, defaulting to 20 if not provided.
- *
- * @param {GetLatestInterviewsParams} params - The parameters for fetching interviews.
- * @param {string} params.userId - The ID of the user whose interviews should be excluded from the results.
- * @param {number} [params.limit=20] - The maximum number of interviews to return; defaults to 20 if not specified.
- * @returns {Promise<Interview[] | null>} A promise that resolves to an array of Interview objects or null if no interviews are found.
- */
+/**
+* Fetches the most recent finalized interviews, excluding those of a specified user.
+*
+* This asynchronous function queries the "interviews" collection in the database, 
+* ordering the results by their creation date in descending order. It filters out 
+* interviews associated with the provided user ID and limits the number of results 
+* based on the specified limit, defaulting to 20 if not provided.
+*
+* @param {GetLatestInterviewsParams} params - The parameters for fetching interviews.
+* @param {string} params.userId - The ID of the user whose interviews should be excluded from the results.
+* @param {number} [params.limit=20] - The maximum number of interviews to return; defaults to 20 if not specified.
+* @returns {Promise<Interview[] | null>} A promise that resolves to an array of Interview objects or null if no interviews are found.
+*/
 export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
     const { userId, limit = 20 } = params;
 
@@ -52,4 +55,53 @@ export async function getInterviewById(id: string): Promise<Interview | null> {
         .get();
 
     return interview.data() as Interview | null;
+}
+
+export async function createFeedBack(params: CreateFeedbackParams) {
+    const { interviewId, userId, transcript } = params;
+
+    try {
+        const formattedTranscript = transcript.map((sentence: { role: string, content: string }) => (
+            `- ${sentence.role}: ${sentence.content}/n`
+        )).join("");
+
+        const { object : {totalScore , categoryScores , strengths , areasForImprovement , finalAssessment} } = await generateObject({
+            model: google("gemini-2.0-flash-001", {
+                structuredOutputs: false
+            }),
+            schema: feedbackSchema,
+            prompt: `
+        You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
+        Transcript:
+        ${formattedTranscript}
+
+        Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
+        - **Communication Skills**: Clarity, articulation, structured responses.
+        - **Technical Knowledge**: Understanding of key concepts for the role.
+        - **Problem-Solving**: Ability to analyze problems and propose solutions.
+        - **Cultural & Role Fit**: Alignment with company values and job role.
+        - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+        `,
+            system:
+                "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
+        });
+
+        const feedback = await db.collection("feedback").add({
+            interviewId,
+            userId,
+            totalScore,
+            categoryScores,
+            strengths,
+            areasForImprovement,
+            finalAssessment,
+            createdAt : new Date().toISOString(),
+        });
+
+        return {
+            success : true,
+            feedbackId: feedback.id
+        };
+    } catch (error) {
+        console.error("Error saving feedback", error);
+    }
 }
